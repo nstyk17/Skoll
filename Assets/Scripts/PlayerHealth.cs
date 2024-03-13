b2764add40241a8bfcf4cf2d4cf46dadf24ac965
nstyk17 <nstyk17@gmail.com>
2024-03-12 18:23:48 -0700

    Health added, interaction script finished

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth, health; 
+
+    void Start()
+    {
+        maxHealth = 10;
+        health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
